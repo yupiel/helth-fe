952bb2ee6f5e4fd814936052cb39f7fce45dba42
Yupiel <stefan_mitrovic@outlook.com>
2021-05-20 10:40:26 +0200

    Fix add mockLocalStorage testing class
    was ignored with last push for unknown reason

---
+++ src/__mocks__/MockLocalStorage.ts
@@ -0,0 +1,27 @@
+class MockLocalStorage {
+	private store: { [key: string]: string } = {};
+
+	public length = Object.keys(this.store).length;
+
+	clear() {
+		this.store = {};
+	}
+
+	getItem(key: string) {
+		return this.store[key] || null;
+	}
+
+	key(index: number) {
+		return Object.keys(this.store)[index];
+	}
+
+	setItem(key: string, value: string) {
+		this.store[key] = String(value);
+	}
+
+	removeItem(key: string) {
+		delete this.store[key];
+	}
+}
+
+export default MockLocalStorage;
